3a61d531d291d3bd7ba13933e42359db899c74fd
Harsh Prajapati <harsh.prajapati@spinny.com>
2022-03-02 15:37:32 +0530

    init commit

--- boxes/settings.py
+++ boxes/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -80,7 +78,10 @@ WSGI_APPLICATION = 'boxes.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -132,5 +133,3 @@ REST_FRAMEWORK = {
 
 
 LOGIN_REDIRECT_URL = 'listall'
-
-django_heroku.settings(locals())
